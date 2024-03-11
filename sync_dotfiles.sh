a4bb6451ee3470cd265186063be806341a957499
yt75534 <>
2024-03-11 11:15:22 +0100

    .

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -8,7 +8,7 @@ cd "$DOTFILES_DIR"
 git pull
 
 # Copy dotfiles
-files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc" "sync_dotfiles.sh" "sync_dotfiles.bat")
+files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc" "sync_dotfiles.sh" "sync_dotfiles_win.sh")
 for file in "${files[@]}"; do
     # Check if the file exists in $HOME_DIR
     if [ -f "$HOME_DIR/$file" ]; then
