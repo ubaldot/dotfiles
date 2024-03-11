7135a68d9db62527cf1721bb444f6a17e68a8999
yt75534 <>
2024-03-11 11:39:17 +0100

    Added clone_dotfiles

---
+++ clone_dotfiles_win.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define paths
+HOME_DIR=/mnt/c/Users/yt75534/
+DOTFILES_DIR="$HOME_DIR/dotfiles"
+DOTVIM_DIR="$HOME_DIR/vimfiles"
+
+cd "$HOME_DIR"
+git clone https://github.com/ubaldot/dotfiles.git
+
+# Copy dotfiles
+files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc" "sync_dotfiles.sh" "sync_dotfiles_win.sh" "clone_dotfiles.sh" "clone_dotfiles_win.sh")
+for file in "${files[@]}"; do
+    # Copy files from ~/dotfiles to ~
+        cp -v "$DOTFILES_DIR/$file" "$HOME_DIR"
+    fi
+done
+
+cp -r "$HOME_DIR/$DOTFILES_DIR/vim/helpme_files/"* "$DOTVIM_DIR/helpme_files"
+cp -r "$HOME_DIR/$DOTFILES_DIR/vim/ftplugin/"* "$DOTVIM_DIR/ftplugin"
+cp -r "$HOME_DIR/$DOTFILES_DIR/vim/lib/"* "$DOTVIM_DIR/lib"
