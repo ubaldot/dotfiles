8f7c97f89b0ac3d5561b48b19a9ec83f85ad8d07
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2024-03-11 19:20:48 +0100

    canceled sync_*

--- clone_dotfiles.sh
+++ clone_dotfiles.sh
@@ -8,17 +8,4 @@ DOTVIM_DIR="$HOME_DIR/.vim"
 cd "$HOME_DIR"
 git clone https://github.com/ubaldot/dotfiles.git
 
-# Copy dotfiles
-files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc" "sync_dotfiles.sh")
-for file in "${files[@]}"; do
-    # Copy files from ~/dotfiles to ~
-        cp -v "$DOTFILES_DIR/$file" "$HOME_DIR"
-done
-
-# Vim
-cp -r "$DOTFILES_DIR/vim/helpme_files/"* "$DOTVIM_DIR/helpme_files"
-cp -r "$DOTFILES_DIR/vim/ftplugin/"* "$DOTVIM_DIR/ftplugin"
-cp -r "$DOTFILES_DIR/vim/lib/"* "$DOTVIM_DIR/lib"
-
-# Manim
-cp -r "$DOTFILES_DIR/manim/"* "$HOME/.manim"
+./read_dotfiles.sh
---
+++ pull_dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define paths
+HOME_DIR=~
+DOTFILES_DIR="$HOME_DIR/dotfiles"
+DOTVIM_DIR="$HOME_DIR/.vim"
+
+cd "$DOTFILES_DIR"
+git pull
+
+# Copy dotfiles
+files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc" "read_dotfiles.sh" "write_dotfiles.sh")
+for file in "${files[@]}"; do
+    # Copy files from ~/dotfiles to ~
+        cp -v "$DOTFILES_DIR/$file" "$HOME_DIR"
+done
+
+# Vim
+cp -r "$DOTFILES_DIR/vim/helpme_files/"* "$DOTVIM_DIR/helpme_files"
+cp -r "$DOTFILES_DIR/vim/ftplugin/"* "$DOTVIM_DIR/ftplugin"
+cp -r "$DOTFILES_DIR/vim/lib/"* "$DOTVIM_DIR/lib"
+
+# Manim
+cp -r "$DOTFILES_DIR/manim/"* "$HOME/.manim"
+
+cd "$HOME_DIR"
