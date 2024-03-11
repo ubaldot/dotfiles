027b6ea450334f8e330891d84625f1298e66ef24
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2024-03-11 09:13:18 +0100

    Update dotfiles: Mon Mar 11 09:13:18 CET 2024

--- read_dotfiles.sh
+++ read_dotfiles.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-cd ~/dotfiles
-git pull
-cp ./.zshrc ~
-cp ./.zprofile ~
-cp ./.vimrc ~
-cp ./.gvimrc ~
-cp ./vim/helpme_files/* ~/.vim/helpme_files
-cp ./vim/ftplugin/* ~/.vim/ftplugin
-cp ./vim/lib/* ~/.vim/lib
-cp ./manim/* ~/.manim
-cp ./read_dotfiles.sh ~
-cp ./write_dotfiles.sh ~
+# Define paths
+DOTFILES_DIR=~/dotfiles
+HOME_DIR=~
+
+# Change directory to dotfiles
+cd "$DOTFILES_DIR" || exit
+
+# Pull changes from Git
+if git pull; then
+    # Copy dotfiles
+    files=(.zshrc .zprofile .vimrc .gvimrc)
+    for file in "${files[@]}"; do
+        cp -v "$file" "$HOME_DIR"
+    done
+
+    # Copy vim files
+    cp -v ./vim/helpme_files/* ~/.vim/helpme_files
+    cp -v ./vim/ftplugin/* ~/.vim/ftplugin
+    cp -v ./vim/lib/* ~/.vim/lib
+
+    # Copy manim files
+    cp -v ./manim/* ~/.manim
+
+    # Copy script files
+    cp -v ./read_dotfiles.sh ./write_dotfiles.sh "$HOME_DIR"
+else
+    echo "Failed to pull changes from Git. Exiting."
+    exit 1
+fi
--- write_dotfiles.sh
+++ write_dotfiles.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-cp ~/.zshrc ~/dotfiles
-cp ~/.zprofile ~/dotfiles
-cp ~/.vimrc ~/dotfiles
-cp ~/.gvimrc ~/dotfiles
-cp ~/.vim/helpme_files/* ~/dotfiles/vim/helpme_files
-cp ~/.vim/ftplugin/* ~/dotfiles/vim/ftplugin
-cp ~/.vim/lib/* ~/dotfiles/vim/lib
-cp ~/.manim/* ~/dotfiles/manim
-cp ~/read_dotfiles.sh ~/dotfiles
-cp ~/write_dotfiles.sh ~/dotfiles
+# Define paths
+DOTFILES_DIR=~/dotfiles
+HOME_DIR=~
 
-cd ~/dotfiles
-git add -u
-git ci -m "Updated some files..."
-git push
+# Backup existing files in dotfiles directory
+mkdir -p "$DOTFILES_DIR/backup"
+cp -v "$DOTFILES_DIR"/* "$DOTFILES_DIR/backup"
+
+# Copy dotfiles
+files=(".zshrc" ".zprofile" ".vimrc" ".gvimrc")
+for file in "${files[@]}"; do
+    cp -v "$HOME_DIR/$file" "$DOTFILES_DIR"
+done
+
+# Copy vim files
+cp -rv "$HOME_DIR/.vim/helpme_files/"* "$DOTFILES_DIR/vim/helpme_files"
+cp -rv "$HOME_DIR/.vim/ftplugin/"* "$DOTFILES_DIR/vim/ftplugin"
+cp -rv "$HOME_DIR/.vim/lib/"* "$DOTFILES_DIR/vim/lib"
+
+# Copy manim files
+cp -rv "$HOME_DIR/.manim/"* "$DOTFILES_DIR/manim"
+
+# Copy script files
+cp -v "$HOME_DIR/read_dotfiles.sh" "$HOME_DIR/write_dotfiles.sh" "$DOTFILES_DIR"
+
+# Change directory to dotfiles
+cd "$DOTFILES_DIR" || exit
+
+# Add all changes to Git
+git add .
+
+# Check if there are changes to commit
+if git diff-index --quiet HEAD --; then
+    echo "No changes to commit. Exiting."
+    exit 0
+fi
+
+# Commit changes
+git commit -m "Update dotfiles: $(date)"
+
+# Push changes to remote repository
+git push origin master
