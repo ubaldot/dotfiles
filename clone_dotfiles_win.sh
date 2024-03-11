ac7dae0bc9b4f24f0092a5dfe14dc7315a75f267
ubaldot <ubaldo.tiberi@gmail.com>
2024-03-11 15:53:39 +0100

    Update clone_dotfiles_win.sh

--- clone_dotfiles_win.sh
+++ clone_dotfiles_win.sh
@@ -9,16 +9,16 @@ cd "$HOME_DIR"
 git clone https://github.com/ubaldot/dotfiles.git
 
 # Copy dotfiles
-files=(".vimrc" ".gvimrc" "sync_dotfiles_win.sh" "clone_dotfiles_win.sh")
+files=(".vimrc" ".gvimrc" "sync_dotfiles_win.sh" )
 for file in "${files[@]}"; do
     # Copy files from ~/dotfiles to ~
         cp -v "$DOTFILES_DIR/$file" "$HOME_DIR"
 done
 
 # Vim
-cp -r "$HOME_DIR/$DOTFILES_DIR/vim/helpme_files/"* "$DOTVIM_DIR/helpme_files"
-cp -r "$HOME_DIR/$DOTFILES_DIR/vim/ftplugin/"* "$DOTVIM_DIR/ftplugin"
-cp -r "$HOME_DIR/$DOTFILES_DIR/vim/lib/"* "$DOTVIM_DIR/lib"
+cp -r "$DOTFILES_DIR/vim/helpme_files/"* "$DOTVIM_DIR/helpme_files"
+cp -r "$DOTFILES_DIR/vim/ftplugin/"* "$DOTVIM_DIR/ftplugin"
+cp -r "$DOTFILES_DIR/vim/lib/"* "$DOTVIM_DIR/lib"
 
 # Manim
-cp -r "$HOME_DIR/$DOTFILES_DIR/manim/"* "$HOME/.manim"
+cp -r "$DOTFILES_DIR/manim/"* "$HOME/.manim"
