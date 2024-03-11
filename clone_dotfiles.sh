d5736002765f76dcab30c1e1e7d2598308bf14f9
ubaldot <ubaldo.tiberi@gmail.com>
2024-03-11 19:32:47 +0100

    Update clone_dotfiles.sh

--- clone_dotfiles.sh
+++ clone_dotfiles.sh
@@ -2,10 +2,8 @@
 
 # Define paths
 HOME_DIR=~
-DOTFILES_DIR="$HOME_DIR/dotfiles"
-DOTVIM_DIR="$HOME_DIR/.vim"
 
 cd "$HOME_DIR"
 git clone https://github.com/ubaldot/dotfiles.git
 
-./pull_dotfiles.sh
+./dotfiles/pull_dotfiles.sh
