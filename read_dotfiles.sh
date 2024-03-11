f11bda7fae39cfe6c0a445e03ced5cde6e8926b8
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2024-03-11 09:05:19 +0100

    .

---
+++ read_dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/dotfiles
+git pull
+cp ./.zshrc ~
+cp ./.zprofile ~
+cp ./.vimrc ~
+cp ./.gvimrc ~
+cp ./vim/helpme_files/* ~/.vim/helpme_files
+cp ./vim/ftplugin/* ~/.vim/ftplugin
+cp ./vim/lib/* ~/.vim/lib
+cp ./manim/* ~/.manim
